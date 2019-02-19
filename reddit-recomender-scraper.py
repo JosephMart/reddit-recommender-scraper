f9d35e877b72e2c7fb0afb9bee013d0261374a20
Joseph Martinsen <joseph@martinsen.com>
2019-02-19 08:22:01 -0600

    60 req per minute?

--- reddit-recomender-scraper.py
+++ reddit-recomender-scraper.py
@@ -3,6 +3,8 @@ import io
 import re
 import logging
 import sys
+import time
+import datetime
 
 import praw
 import click
@@ -17,7 +19,8 @@ reddit_instance = praw.Reddit(client_id=os.environ['client_id'],
                               client_secret=os.environ['client_secret'],
                               username=os.environ['username'],
                               password=os.environ['password'],
-                              user_agent='Fetching data for subreddit recomendations')
+                              user_agent='Fetching data for subreddit recomendations',
+                              api_request_delay=2)
 
 
 @click.group()
@@ -25,42 +28,57 @@ def cli():
     pass
 
 
-def setupRunner(line: str):
-    x = regex_subreddit.findall(line)
-
-    if len(x) != 1:
-        logging.error(f'Error with {line}', end='')
-        return '', 0
-
-    try:
-        subscribers = reddit_instance.subreddit(x[0]).subscribers
-    except Exception as e:
-        logging.error(f'Error with {line}', end='')
-        return x[0], 0
-    logging.info(f'{x[0]}-{subscribers}')
-    return x[0], subscribers
+def wait_until(execute_it_now):
+    while True:
+        diff = (execute_it_now - datetime.datetime.now()).total_seconds()
+        if diff <= 0:
+            return
+        elif diff <= 0.1:
+            time.sleep(0.001)
+        elif diff <= 0.5:
+            time.sleep(0.01)
+        elif diff <= 1.5:
+            time.sleep(0.1)
+        else:
+            time.sleep(1)
 
 
 @click.command()
 @click.option('--file', type=click.File('r'))
-@click.option('--workers', default=5, type=int)
-def setup(file: io.TextIOWrapper, workers: int):
+def setup(file: io.TextIOWrapper):
     logging.info('Setup starting')
-    pool = ThreadPool(workers)
-    results = pool.map(setupRunner, file)
-
-    # close the pool and wait for the work to finish
     try:
-        pool.close()
-        pool.join()
+        with open('cleanSubreddits.txt', 'w') as out:
+            calls_this_min = 0
+            oldtime = time.time()
+
+            for line in file:
+                if time.time() - oldtime > 59:
+                    calls_this_min = 0
+                    oldtime = time.time()
+                elif calls_this_min >= 60:
+                    logging.info('waiting...')
+                    wait_until(datetime.datetime.fromtimestamp(oldtime + 60))
+                    calls_this_min = 0
+                    oldtime = time.time()
+
+                x = regex_subreddit.findall(line)
+                if len(x) != 1:
+                    logging.error(f'Error with {line}')
+                    continue
+
+                try:
+                    calls_this_min += 1
+                    subscribers = reddit_instance.subreddit(x[0]).subscribers
+                except Exception as e:
+                    logging.error(f'Error with {line}')
+                    continue
+                logging.info(f'{x[0]}-{subscribers}')
+                out.write(f'{x[0]}-{subscribers}\n')
+
     except KeyboardInterrupt:
         logging.info('Keyboard interupt')
         sys.exit(1)
-
-    with open('cleanSubreddits.txt', 'w') as out:
-        results.sort(key=lambda tup: tup[1], reverse=True)
-        for r in results:
-            out.write(f'{r[0]}\n')
 
 
 @click.command()
